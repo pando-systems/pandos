60a404ba8d84d5b5522b0af2132c83d9d593a63c
rhdzmota <contact@rhdzmota.com>
2023-10-11 14:02:01 -0600

    PAN-1 Scaffold for the pandos_core module

---
+++ src/pandos_core/setup.py
@@ -0,0 +1,64 @@
+import os
+from setuptools import setup, find_namespace_packages
+from typing import (
+    List,
+)
+
+
+with open("README.md", "r") as file:
+    readme = file.read()
+
+with open("requirements.txt", "r") as file:
+    requirements = [req for req in file.read().split("\n") if req and not req.startswith("#")]
+
+with open(os.path.join("pandos", "version"), "r") as file:
+    version = file.read().strip()
+
+with open("LICENSE", "r") as file:
+    license_content = file.read()
+
+
+def get_packages(here: str = ".", pandos_prefix: str = "pandos") -> List[str]:
+    return [
+        package
+        for package in find_namespace_packages(where=here)
+        if package.startswith(pandos_prefix)
+    ]
+
+
+setup(
+    name="pandos",
+    version=version,
+    description=(
+        "The `pandos core` package contains the baseline implementation of the pandos base abstractions."
+    ),
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    author="rhdzmota",
+    author_email="info@rhdzmota.com",
+    url="rhdzmota.com/pandos",
+    # https://pypi.org/classifiers/
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Typing :: Typed",
+        "Topic :: Scientific/Engineering",
+        "Topic :: System :: Distributed Computing",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.10",
+    ],
+    package_data={
+        "": [
+            "version",
+        ]
+    },
+    include_package_data=True,
+    packages=get_packages(),
+    scripts=[
+        "bin/pandos"
+    ],
+    install_requirements=requirements,
+    python_requires=">=3.10, <4",
+    license=license_content,
+)
