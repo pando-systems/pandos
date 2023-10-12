0842389e282ad7b6d26411e8127ad97a78177e23
rhdzmota <contact@rhdzmota.com>
2023-10-11 21:30:08 -0600

    PAN-5 Add test scenarios for the either-monad

---
+++ pandos/src/tests/test_pandos/test_monads/test_either.py
@@ -0,0 +1,54 @@
+from pandos.monads import Either, Right, Left
+
+
+class CustomTestError(Exception):
+
+    @classmethod
+    def raise_error(cls, *args, **kwargs):
+        raise cls(*args, **kwargs)
+
+
+def test_either_from_value():
+    value = 5
+    instance = Either.from_value(x=value)
+    assert instance.map(lambda x: x == value).resolve()
+
+
+def test_either_right_map():
+    def add_one(x) -> int:
+        return x + 1
+    value = 5
+    either_in = Either.from_value(x=value)
+    either_out = either_in.map(add_one)
+    assert isinstance(either_out, Right)
+    assert either_out.resolve() == add_one(either_in.resolve())
+
+
+def test_either_left_map():
+    def zero_div(x):
+        return x / 0
+
+    value = 5
+    either_in = Either.from_value(x=value)
+    either_out = either_in.map(zero_div)
+    try:
+        zero_div(value)
+        raise ValueError("Transformation should've failed")
+    except ZeroDivisionError as e:
+        assert isinstance(either_out, Left)
+
+
+def test_either_sticky_left():
+    either_in = Either.from_value(x=0)\
+        .map(lambda x: CustomTestError.raise_error())
+    either_out = either_in.map(lambda x: x + 1)
+    assert isinstance(either_in, Left)
+    assert isinstance(either_out, Left)
+
+
+def test_either_right_flatmap():
+    pass
+
+
+def test_either_left_flatmap():
+    pass
