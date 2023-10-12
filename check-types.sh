b1c263e0b32d265eabd9427a13cd6e09e5143806
rhdzmota <contact@rhdzmota.com>
2023-10-11 18:39:33 -0600

    PAN-3 Update check-* scripts to reference new layout

--- check-types.sh
+++ check-types.sh
@@ -1,1 +1,1 @@
-(mkdir .mypy_cache; mypy src/ --ignore-missing-imports --install-types --non-interactive --explicit-package-bases)
+(mkdir .mypy_cache; mypy pandos/ --ignore-missing-imports --install-types --non-interactive --explicit-package-bases)
