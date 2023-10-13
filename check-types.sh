5ac15414aa46574112e18b20f14239ebe5dec481
rhdzmota <contact@rhdzmota.com>
2023-10-12 23:25:48 -0600

    PAN-10 Check only source-code types

--- check-types.sh
+++ check-types.sh
@@ -1,1 +1,1 @@
-(mkdir .mypy_cache; mypy pandos/ --ignore-missing-imports --install-types --non-interactive --explicit-package-bases)
+(mkdir .mypy_cache; mypy pandos/src --ignore-missing-imports --install-types --non-interactive --explicit-package-bases)
