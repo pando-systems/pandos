e1afe80ae22e6a292b3fda53ea1941171afb292e
rhdzmota <contact@rhdzmota.com>
2023-10-15 19:23:10 -0600

    PAN-17 Add first simple version of logger utils

--- pandos/src/main/pandos/settings.py
+++ pandos/src/main/pandos/settings.py
@@ -1,1 +1,10 @@
 import os
+import logging
+
+
+def get_logger(name: str):
+    logging.basicConfig(
+        encoding="utc-8",
+        level=logging.INFO,
+    )
+    return logging.getLogger(name)
