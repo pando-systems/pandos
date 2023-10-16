720985d20b74ce0257ca23f666198a0b4db69793
rhdzmota <contact@rhdzmota.com>
2023-10-16 16:29:16 -0600

    PAN-17 Allow disabling feature maturity logs

--- pandos/src/main/pandos/settings.py
+++ pandos/src/main/pandos/settings.py
@@ -2,7 +2,13 @@ import os
 import logging
 
 
-def get_logger(name: str):
+PANDOS_DISABLE_FEATURE_MATURITY_LOGS = int(os.environ.get(
+    "PANDOS_DISABLE_FEATURE_MATURITY_LOGS",
+    default="0"
+))
+
+
+def get_logger(name: str) -> logging.Logger:
     logging.basicConfig(
         encoding="utc-8",
         level=logging.INFO,  # TODO: Parametrize log-level
